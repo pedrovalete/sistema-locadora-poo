a6b2d4ac6c36ca05b8c2797594a0c92b2300c3fa
Pedro Valete <pedrovalete06@gmail.com>
2025-10-14 13:18:49 -0300

    Começando a mexer na classe Cliente, construtor e getters feitos, com atualizarCadastro e autenticar a senha

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -21,14 +21,29 @@ public class App {
         boolean sair = false;
         while(!sair){
             System.out.println("\n--- MENU ADMINISTRADOR ---");
-            System.out.println("1. Cadastrar Cliente");
-            System.out.println("2. Cadastrar Jogo");
+            System.out.println("1. Gerenciar Clientes");
+            System.out.println("2. Gerenciar Jogos");
+            System.out.println("3. Gerenciar Plataformas");
+            System.out.println("4. Gerenciar Locações");
+            System.out.println("5. Gerenciar Aluguéis");
             System.out.println("0. Sair");
 
             int escolha = sc.nextInt();
 
             switch(escolha){
+                case 1:
+                    System.out.println("\n--- GERENCIAR CLIENTES ---");
+                    System.out.println("1. Cadastrar Cliente");
+                    System.out.println("2. Atualizar Cliente");
+                    System.out.println("3. Listar Clientes");
+                    System.out.println("4. Remover Cliente");
+                    System.out.println("0. Sair");
+                    int gerenciarCliente = sc.nextInt();
 
+                    switch(gerenciarCliente){
+                        case 1:
+
+                    }
             }
         }
     }
--- app/src/main/java/org/example/domain/Cliente.java
+++ app/src/main/java/org/example/domain/Cliente.java
@@ -1,11 +1,53 @@
 package org.example.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private int id;
     private String nome;
     private String email;
     private String telefone;
     private String senha;
-    private AluguelConsole alugueis;
-    private LocacaoJogo locacoes;
+
+    private List<AluguelConsole> alugueis;
+    private List<LocacaoJogo> locacoes;
+
+    public Cliente(int id, String nome, String email, String telefone, String senha){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+
+        this.locacoes = new ArrayList<>();
+        this.alugueis = new ArrayList<>();
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public String getSenha(){
+        return senha;
+    }
+
+    public void atualizarCadastro(String nome, String email, String telefone, String senha){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+    
+    public boolean autenticar(String senha){
+        return this.senha.equals(senha);
+    }
 }
